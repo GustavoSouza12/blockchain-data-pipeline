dd0701e38ed6a3ce8e8f7b8151fc4f24d1be060e
Gustavo Souza <Gustavosouzaalves12@gmail.com>
2026-05-14 20:59:22 -0300

    postgress connection created

--- etl/ingest_data.py
+++ etl/ingest_data.py
@@ -15,7 +15,7 @@ env_path = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
 
-# DEBUG
+
 print("ENV PATH:", env_path)
 print("HOST:", os.getenv("DB_HOST"))
 print("PORT:", os.getenv("DB_PORT"))
